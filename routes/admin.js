8b7193645cb94df29b35ba04ba75c5efae7e475f
IgweDaniel <danielchibuzoigwe@gmail.com>
2021-05-06 12:22:16 +0100

    styled and implemented logic for  pending orders and deposits page

--- routes/admin.js
+++ routes/admin.js
@@ -9,8 +9,75 @@ const router = express.Router();
 
 router.use(requiresAdmin);
 
-router.get("/", async function (req, res, next) {
-  res.render("admin/index", { title: "admin" });
-});
+router
+  .route("/")
+  .get(async function (req, res, next) {
+    console.log(req.body);
+    res.render("admin/index", { title: "admin" });
+  })
+  .post(async function (req, res) {
+    console.log(req.body);
+    return res.redirect("/admin/");
+  });
+
+router
+  .route("/withdrawal_request")
+  .get(async function (req, res, next) {
+    const pendingWithdrawals = await Transaction.find({
+      type: "withdraw",
+      status: "PENDING",
+    })
+      .populate("user")
+      .lean()
+      .exec();
+
+    res.render("admin/withdrawal_request", {
+      title: "admin",
+      entries: pendingWithdrawals,
+      keys: [
+        "username",
+        "email",
+        "amount",
+        "currency",
+        "status",
+        "transactionId",
+        "created_on",
+      ],
+    });
+  })
+  .post(async function (req, res) {
+    console.log(req.body);
+    return res.redirect("/admin/withdrawal_request");
+  });
+
+router
+  .route("/deposited_orders")
+  .get(async function (req, res, next) {
+    const pendingWithdrawals = await Transaction.find({
+      type: "deposit",
+      status: "PENDING",
+    })
+      .populate("user")
+      .lean()
+      .exec();
+
+    res.render("admin/deposited_orders", {
+      title: "admin",
+      entries: pendingWithdrawals,
+      keys: [
+        "username",
+        "email",
+        "amount",
+        "currency",
+        "status",
+        "transactionId",
+        "created_on",
+      ],
+    });
+  })
+  .post(async function (req, res) {
+    console.log(req.body);
+    return res.redirect("/admin/deposited_orders");
+  });
 
 module.exports = router;
